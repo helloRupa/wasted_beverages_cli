405c9a4c2a214bcf7e986fa73fa66138aed0fd10
LimitToInfinity <21stLimitless@gmail.com>
2020-07-24 15:07:16 -0600

    Comments added around Hooks.make_action for learning.

--- lib/modules/hooks.rb
+++ lib/modules/hooks.rb
@@ -41,8 +41,20 @@ module Hooks
     prepend preceder
   end
 
+  # I can't find a way to figure out the context of
+  # which the Hooks.method gets called from.
+  # Like if I put a binding.pry inside of
+  # my def self.make_action(action, instance)
+  # I can't find out that a Cli instance was
+  # the previous context before this method was entered
   def self.make_action(action, instance)
     is_sym_or_str = action.is_a?(Symbol) || action.is_a?(String)
+
+    # is there any other way to make a method from that symbol (e.g. :welcome)
+    # on that instance without passing
+    # in the instance (e.g. #<Cli:109471207asfaf>)
     is_sym_or_str ? instance.method(action) : action
+    # I can't find any reason to use self.class.method(action).bind(self)
+    # or really any way to use it that isn't convoluted.
   end
 end
